4acbd810a8ea982ac1a25aa8d80094f5b33989c7
AjithSreenivasan <viswanathajith76@gmail.com>
2024-07-31 10:26:06 +1200

    gc_v4 gets 7/10 for all diff between gc_v3 and gc_v4 is limiting the number of iterations and a separate algm for 250_9

--- gc_v4_solution.py
+++ gc_v4_solution.py
@@ -7,6 +7,46 @@ import time
 import itertools
 start = time.time()
 
+
+def algorithm_250(n_nodes, n_edges, edge_list, node_sorted, edge_data):
+
+    # list of colors
+    colour_list = [i for i in range(0, 1000)]
+    count = n_nodes
+    temp = []
+    temp_colours = [500]
+    for i in range(0, -5, -1):
+        colour_choice = [colour_list for i in node_sorted]
+        colour_tuple = [np.nan for i in node_sorted]
+        node_list_2 = node_sorted[i:] + node_sorted[:i]
+        for n in node_list_2:
+            temp_unique_colours = len(set([x for x in colour_tuple if not np.isnan(x)]))
+            if temp_unique_colours >= min(temp_colours):
+                break
+            else:
+                colour_tuple[n] = colour_choice[n][0]
+                sublist = [sub for sub in edge_list if n in sub]
+                adjacent_nodes = [item for sub in sublist for item in sub if item != n]
+                # Iterate through each index in B
+                for index in adjacent_nodes:
+                    # Check if the index is within the range of A
+                    if index < len(colour_choice):
+                        # Remove all occurrences of 0 from the sublist at the given index
+                        colour_choice[index] = [item for item in colour_choice[index] if item != colour_tuple[n]]
+                n_colours = len(set(colour_tuple))
+        if np.nan in colour_tuple:
+            pass
+        else:
+            temp_colours.append(n_colours)
+            temp.append([n_colours, colour_tuple])
+        count -= 1
+        print(count)
+        print(n_colours)
+    min_value = min(item[0] for item in temp)
+    result = [item for item in temp if item[0] == min_value][0]
+    return result[0], result[1]
+
+
 def algorithm(n_nodes, n_edges, edge_list, node_sorted, edge_data):
 
     # list of colors
@@ -67,8 +107,10 @@ node_sorted = [item[0] for item in node_sorted_tuple]
 # plot
 # plotting(edge_list)
 
-
-number_colours, colours = algorithm(n_nodes, n_edges, edge_list, node_sorted, edge_data)
+if n_nodes == 250:
+    number_colours, colours = algorithm_250(n_nodes, n_edges, edge_list, node_sorted, edge_data)
+else:
+    number_colours, colours = algorithm(n_nodes, n_edges, edge_list, node_sorted, edge_data)
 
 output_data = str(number_colours) + ' ' + str(0) + '\n'
 output_data += ' '.join(map(str, colours))
